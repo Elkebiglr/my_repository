1b540971b4bded1c0281f21f8ae21f1b1a7e60a9
EPF van den Biggelaar <e.p.f.v.d.biggelaar@student.tue.nl>
2019-09-08 15:25:17 +0200

    Added __repr()__ method

--- vector.py
+++ vector.py
@@ -6,5 +6,12 @@ Created on Sun Sep  8 15:12:34 2019
 """
 
 class Vector:
-        def __init__(self, *elements):
-                self.elements = elements
+    def __init__(self, *elements):
+        self.elements = elements
+
+    def __repr__(self):
+        s = '['
+        for x in self.elements:
+            s += str(x) + ', '
+        s += ']'
+        return s
