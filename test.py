2d2ec2e878ad058f95d11897fc5d36aabac576bb
EPF van den Biggelaar <e.p.f.v.d.biggelaar@student.tue.nl>
2019-09-08 15:57:51 +0200

    Added test.py

---
+++ test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  8 15:54:42 2019
+
+@author: s120264
+"""
+
+from vector import Vector
+v1 = Vector(1,2,3)
+v2 = Vector(3,2,1)
+print(v1)
+print(len(v2))
+print(v1+v2)
