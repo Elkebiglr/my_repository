76df00298384bffc2911b6c5326018db00c7e625
EPF van den Biggelaar <e.p.f.v.d.biggelaar@student.tue.nl>
2019-09-08 15:18:06 +0200

    'aaaa'

---
+++ vector.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep  8 15:12:34 2019
+
+@author: s120264
+"""
+
+class Vector:
+        def __init__(self, *elements):
+                self.elements = elements
